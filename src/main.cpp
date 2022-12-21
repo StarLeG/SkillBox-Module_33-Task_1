cf2a4b27c15b9217665895eac56e162008d4bea3
Oleg Starinsky <oleg.starynski@gmail.com>
2022-12-21 08:21:55 +0300

    ADD metod menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include "base.h"
+#include "menu.h"
 
 int main()
 {
-	Object modex;
+	int input = -1;
+	menu();
+	while (input != 0)
+	{
+		switch (input)
+		{
+		case 0:
+			input = 0;
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			break;
+		}
 
-	modex.addBase(modex.creatArticle(), 5);
+	}
 
-	modex.addBase("assds",5);
-
-
-	modex.showBase();
-	
-
-	
 	return 0;
 }
---
+++ src/menu.cpp
@@ -0,0 +1,14 @@
+#include "menu.h"
+
+void menu()
+{
+	std::cout << "Welcome to MODEX online store." << std::endl;
+	std::cout << "Menu:" << std::endl;
+	std::cout << "1 - Add a product to the store database;" << std::endl;
+	std::cout << "2 - Add item to cart;" << std::endl;
+	std::cout << "3 - Display products from the store database;" << std::endl;
+	std::cout << "4 - Display an item from the shopping cart;" << std::endl;
+	std::cout << "5 - Automatic addition of goods to the store database;" << std::endl;
+	std::cout << "0 - EXIT" << std::endl;
+
+}
